20003e7356ce91306a66ffe5f99b2aecc6f3c5e0
Ryan Smith <ryan.l.smith142@gmail.com>
2021-08-16 17:31:23 -0500

    Playing around with state and props

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,31 +1,47 @@
 import React, { Component } from 'react';
 
 export default class AppContent extends Component {
+
+    state = {posts: []}
+
+    constructor(props) {
+        super(props);
+        this.listRef = React.createRef();
+    }
+
     fetchList = () => {
         fetch('https://jsonplaceholder.typicode.com/posts')
             .then((response) => response.json())
             .then(json => {
-                console.log(json);
-                let posts = document.getElementById("post-list");
-
-                json.forEach(function(obj) {
-                    let li = document.createElement("li")
-                    li.appendChild(document.createTextNode(obj.title));
-                    posts.appendChild(li);
-                })
+                this.setState({posts: json});
             })
+    }
+    clickedItem = (x) => {
+        console.log("Clicked", x);
     }
     render() {
         return (
-            <p>
+            <div>
                 This is the content.
                 <br />
+                <hr />
+                <p onMouseEnter={this.anotherFunction}>This is some text.</p>
+
                 <button onClick={this.fetchList} className="btn btn-primary" href='#'>Fetch Data</button>
 
                 <hr />
 
-                <ul id="post-list"></ul>
-            </p>
+                <p>Posts is {this.state.posts.length} items long</p>
+                <ul>
+                    {this.state.posts.map((c) => (
+                        <li key={c.id}>
+                            <a href="#!" onClick={() => this.clickedItem(c.id)}>
+                                {c.title}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         );
     }
 }
