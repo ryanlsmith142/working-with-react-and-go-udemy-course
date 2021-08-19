cab24b8fff5b892f8adfcd7f715f6f3a49467a3c
Ryan Smith <ryan.l.smith142@gmail.com>
2021-08-19 08:10:17 -0500

    Adds some movies and sets them in state

--- go-movies/src/components/Movies.js
+++ go-movies/src/components/Movies.js
@@ -1,9 +1,32 @@
-import React, {Component} from 'react';
+import React, { Component, Fragment } from 'react';
 
 export default class Movies extends Component {
+
+    state = { movies: []};
+
+    componentDidMount() {
+        this.setState({
+            movies: [
+                {id: 1, title: "The Shawshank Redemption", runtime: 142 },
+                {id: 2, title: "The Godfather", runtime: 175 },
+                {id: 3, title: "The Dark Knight", runtime: 153 },
+            ]
+        })
+    }
+
     render() {
         return (
+            <Fragment>
             <h2>Choose a movie</h2>
-        )
+
+            <ul>
+                {this.state.movies.map( (m) => (
+                    <li key={m.id}>
+                        {m.title}
+                    </li>
+                ))}
+            </ul>
+            </Fragment>
+        );
     }
 }
