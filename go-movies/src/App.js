bd7bd21a1cd38d8ce1be69e57191adbf4fed0df1
Ryan Smith <ryan.l.smith142@gmail.com>
2021-08-20 07:19:00 -0500

    Adds single movie

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -4,6 +4,7 @@ import Movies from './components/Movies';
 import Admin from './components/Admin'
 import Home from './components/Home'
 import Categories from './components/Categories';
+import OneMovie from './components/OneMovie';
 
 export default function App() {
   return (
@@ -39,9 +40,7 @@ export default function App() {
 
       <div className="col-md-10">
         <Switch>
-          <Route path="/movies/:id">
-            <Movie />
-          </Route>
+          <Route path="/movies/:id" component={OneMovie} />
 
           <Route path="/movies">
             <Movies />
@@ -78,12 +77,6 @@ export default function App() {
     </div>
     </Router>
   );
-}
-
-function Movie() {
-  let { id } = useParams();
-  
-  return <h2>Movie id {id}</h2>
 }
 
 function CategoryPage() {
