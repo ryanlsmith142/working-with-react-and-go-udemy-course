bfc8fac72a0d1ae8318d40651590b8c881165c83
Ryan Smith <ryan.l.smith142@gmail.com>
2021-08-19 19:19:47 -0500

    Work with react routing

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react'
-import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} from 'react-router-dom';
 import Movies from './components/Movies';
 import Admin from './components/Admin'
 import Home from './components/Home'
+import Categories from './components/Categories';
 
 export default function App() {
   return (
@@ -27,6 +28,9 @@ export default function App() {
               <Link to="/movies">Movies</Link>
             </li>
             <li className="list-group-item">
+              <Link to="/by-category">Categories</Link>
+            </li>
+            <li className="list-group-item">
               <Link to="/admin">Admin</Link>
             </li>
           </ul>
@@ -35,19 +39,64 @@ export default function App() {
 
       <div className="col-md-10">
         <Switch>
+          <Route path="/movies/:id">
+            <Movie />
+          </Route>
+
           <Route path="/movies">
             <Movies />
           </Route>
+
+          <Route exact path="/by-category">
+            <CategoryPage />
+          </Route>
+
+          <Route 
+          exact 
+          path="/by-category/drama" 
+          render={(props) => <Categories {...props} title={`Drama`} />}
+          /> 
+
+          <Route 
+          exact 
+          path="/by-category/comedy" 
+          render={(props) => <Categories {...props} title={`Comedy`} />}
+          /> 
+            
           <Route path="/admin">
             <Admin />
           </Route>
+
           <Route path="/">
             <Home />
           </Route>
+
+          
         </Switch>
       </div>
     </div>
     </div>
     </Router>
+  );
+}
+
+function Movie() {
+  let { id } = useParams();
+  
+  return <h2>Movie id {id}</h2>
+}
+
+function CategoryPage() {
+
+  let{ path, url } = useRouteMatch();
+  return (
+    <div>
+      <h2>Categories</h2>
+
+      <ul>
+        <li><Link to={`${path}/comedy`}>Comedy</Link></li>
+        <li><Link to={`${path}/drama`}>Drama</Link></li>
+      </ul>
+    </div>
   );
 }
