0e46692acadc744fe5effac3515f807d8556cbae
Ryan Smith <ryansmith@Ryans-MBP.attlocal.net>
2021-09-07 19:19:36 -0500

    Adds stub functions

--- backend-app/cmd/api/movies-handler.go
+++ backend-app/cmd/api/movies-handler.go
@@ -18,8 +18,6 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.logger.Println("id is", id)
-
 	movie, err := app.models.DB.Get(id)
 
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
@@ -42,3 +40,21 @@ func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+
+}
+
+
