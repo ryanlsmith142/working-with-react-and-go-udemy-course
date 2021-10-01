d6e96b7e70bda67295e6fbabc733587f8a92cf86
Ryan Smith <ryan.l.smith142@gmail.com>
2021-09-30 21:27:03 -0500

    Adds list to appear with data on click

--- src/AppContent.js
+++ src/AppContent.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 
 export default class AppContent extends Component {
+    fetchList = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then((response) => response.json())
+            .then(json => {
+                console.log(json);
+                let posts = document.getElementById("post-list");
+
+                json.forEach(function(obj) {
+                    let li = document.createElement("li")
+                    li.appendChild(document.createTextNode(obj.title));
+                    posts.appendChild(li);
+                })
+            })
+    }
     render() {
         return (
             <p>
                 This is the content.
                 <br />
-                <button className="btn btn-primary" href='#'>My Button</button>
+                <button onClick={this.fetchList} className="btn btn-primary" href='#'>Fetch Data</button>
+
+                <hr />
+
+                <ul id="post-list"></ul>
             </p>
         );
     }
