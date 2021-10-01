ec2fc03c5e2a1bf21ee2a19a19d8ac3cd6a46f96
Ryan Smith <ryan.l.smith142@gmail.com>
2021-09-30 21:27:03 -0500

    Adds some movies and sets them in state

--- go-movies/src/App.js
+++ go-movies/src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react'
 import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Movies from './components/Movies';
+import Admin from './components/Admin'
+import Home from './components/Home'
 
 export default function App() {
   return (
@@ -48,12 +50,4 @@ export default function App() {
     </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>
-}
-
-function Admin() {
-  return <h2>Manage Catalague</h2>
 }
