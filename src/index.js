2d791ee487d712f1aa030ba5dee14592b7f4014d
Ryan Smith <ryan.l.smith142@gmail.com>
2021-09-30 21:27:03 -0500

    Initial setup and start of lecture

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import AppFooter from './AppFooter';
+import AppContent from './AppContent';
+import AppHeader from './AppHeader';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends Component {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  render() {
+    return (
+    <div className="app">
+        <AppHeader title="My App" />
+        <AppContent />
+        <AppFooter />
+    </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
