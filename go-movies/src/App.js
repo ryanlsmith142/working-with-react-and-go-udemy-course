af09b026b020f65c9993c015e97893a30c516013
Ryan Smith <ryan.l.smith142@gmail.com>
2021-09-30 21:27:03 -0500

    Initial project setup

---
+++ go-movies/src/App.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react'
+import {HashRouter as Router, Switch, Route, Link} from 'react-router-dom';
+import Movies from './components/Movies';
+
+export default function App() {
+  return (
+    <Router>
+    <div className="container">
+
+    <div className="row">
+      <h1 className="mt-3">
+        Go Watch a Movie!
+      </h1>
+      <hr className="mb-3"></hr>
+    </div>
+
+    <div className="row">
+      <div className="col-md-2">
+        <nav>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <Link to="/">Home</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/movies">Movies</Link>
+            </li>
+            <li className="list-group-item">
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div className="col-md-10">
+        <Switch>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/admin">
+            <Admin />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+    </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <h2>Home</h2>
+}
+
+function Admin() {
+  return <h2>Manage Catalague</h2>
+}
