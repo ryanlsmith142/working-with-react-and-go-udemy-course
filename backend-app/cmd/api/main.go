48f8d43dea0b9963d1f03cd677c7aa970d42010c
Ryan Smith <ryan.l.smith142@gmail.com>
2021-11-03 19:31:56 -0500

    Adds local dsn var for windows setup

--- backend-app/cmd/api/main.go
+++ backend-app/cmd/api/main.go
@@ -41,11 +41,7 @@ func main() {
 
 	flag.IntVar(&cfg.port, "port", 4000, "Server port to listen on")
 	flag.StringVar(&cfg.env, "env", "development", "Application environment (development | production ")
-
-
 	flag.StringVar(&cfg.db.dsn, "dsn", localDSN, "Postgres connection string")
-
-
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
